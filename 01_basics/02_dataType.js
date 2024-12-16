7e1f6488f6313903364c6b4a41e7e4bc97304391
Niyati Patel <niyatihpatel0403@gmail.com>
2024-12-16 17:58:33 +0000

    dataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS as newer version
+
+// alert("3 + 3") // we are using node.js ,not browser
+
+console.log(3 
+    + 
+    3) //code readability should be high
+
+
+console.log("Niyati")
+
+
+let name = "Niyati" 
+let age = 18
+let isloggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefind => 
+// symbol => unique
+
+//object
+
+console.log(typeof "Niyati");//string
+console.log(typeof null);//object
+console.log(typeof age);//number
+console.log(typeof undefined);//undefind
+
+
