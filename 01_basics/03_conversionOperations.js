e34673910dd49741d0c03ab9b3502d9c9690a1f9
Niyati Patel <niyatihpatel0403@gmail.com>
2024-12-16 19:34:28 +0000

    conversion datatypes

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -75,3 +75,58 @@ let someNumber = 33
 let stringNumber = String(someNumber);
 console.log(typeof stringNumber); //string
 console.log(stringNumber);//33
+
+//*********************************************** Operations ********************************************************************/
+
+let value = 3
+let negValue = -value
+//console.log(negValue);//-3
+
+//console.log(2+2);//4
+//console.log(2-2);//0
+//console.log(2*2);//4
+//console.log(2**3);//8
+//console.log(2/3);//0.6666666
+//console.log(2%3);//2
+
+let str1 = "hello"
+let str2 = " Niyati"
+
+let str3 = str1 + str2
+console.log(str3);//hello Niyati
+
+// console.log("1" + 2);//12
+// console.log(1 + "2");//12
+// console.log("1" + "2");//12
+// console.log("1" + 2 + 2);//122
+// console.log(1 + 2 + "2");//32
+
+// console.log( (3+ 4) * 5 % 3);//2
+
+console.log(+true);//1
+console.log(true);//true
+console.log(+"");//0
+
+let num1 , num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter;// ++gameCounter => 101
+console.log(gameCounter);//100
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
